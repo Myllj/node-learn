1c465c38c366b0b37b1f0e50af228af8701493aa
llj19930620 <llj1993hehe@163.com>
2022-08-09 15:55:46 +0800

    大事件项目个人中心模块

--- case/api_server/app.js
+++ case/api_server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const joi = require('joi')
 
 // 配置cors跨域
 const cors = require('cors')
@@ -8,11 +9,44 @@ app.use(cors())
 //配置解析 application/x-www-form-urlencoded 格式的表单数据的中间件
 app.use(express.urlencoded({ extended: false }))
 
+//优化 res.send() 代码
+// 响应数据的中间件
+app.use(function (req, res, next) {
+  // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
+  res.cc = function (err, status = 1) {
+    res.send({
+      // 状态
+      status,
+      // 状态描述，判断 err 是 错误对象 还是 字符串
+      message: err instanceof Error ? err.message : err,
+    })
+  }
+  next()
+})
 
-const router=require('./router/user')
-app.use('/api',router)
+// 解析 token 的中间件
+const expressJWT = require('express-jwt')
+// 导入配置文件
+const config = require('./config')
+// 使用 .unless({ path: [/^\/api\//] }) 指定哪些接口不需要进行 Token 的身份认证
+app.use(expressJWT({ secret: config.jwtSecretKey }).unless({ path: [/^\/api\//] }))
 
+// 导入并使用用户路由模块
+const userRouter = require('./router/user')
+app.use('/api', userRouter)
+// 导入并使用用户信息的路由模块
+const userinfoRouter = require('./router/userinfo')
+app.use('/my', userinfoRouter)
 
+// 定义错误级别的中间件
+app.use((err, req, res, next) => {
+  // 验证失败导致的错误
+  if (err instanceof joi.ValidationError) return res.cc(err)
+  // 身份认证失败后的错误
+  if (err.name === 'UnauthorizedError') return res.cc('身份认证失败！')
+  // 未知的错误
+  res.cc(err)
+})
 
 app.listen(3007, function () {
   console.log('api server running at http://127.0.0.1:3007')
