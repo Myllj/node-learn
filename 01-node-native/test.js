a837f6f23ca944bdb0f54dc82d67b8c9c486c0b0
Myllj <llj1993hehe@163.com>
2026-05-24 15:21:50 +0800

    多年前node学习遗留的代码的一些文件夹重命名

---
+++ 01-node-native/test.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: llj
+ * @email: 
+ * @Date: 2026-04-13 14:49:33
+ * @LastEditors: llj
+ * @LastEditTime: 2026-04-13 14:51:01
+ */
+console.log(global.process.env.node_env);
